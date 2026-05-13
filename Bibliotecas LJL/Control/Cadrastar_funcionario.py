669575a51cad930017c59c64249187d22cdb9f46
thalesxzzl <to1658524@gmail.com>
2026-05-12 21:57:23 -0300

    biblioteca

---
+++ Bibliotecas LJL/Control/Cadrastar_funcionario.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from Conexao import conecta
+
+def cadastrar_funcionarios():
+    nome = input("qual o nome dofuncionario que voce deseja cadastrar?")
+    cpf =input("")
+
+
+
+
+
+
+conecta()
+
+
+
+
+
+
+
+
+
---
+++ Bibliotecas LJL/Control/Conexao.py
@@ -0,0 +1,36 @@
+import mysql.connector
+def conecta():
+    global conexao
+    #executa uma funçao da lib que realiza a conexao
+    conexao = mysql.connector.connect(
+        #parametro de conexao o banco de dados
+        host ="localhost",
+        user="root",
+        password="",
+        database = "biblioteca"
+    )
+    print ("conectado")
+    return conexao
+
+
+conecta()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
